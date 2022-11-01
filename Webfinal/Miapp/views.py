0a1e4c03f71c1d10dedf5eab769f432ea60c22d8
Adrian Pozzi <adrianpozzi@gmail.com>
2022-11-01 10:35:51 -0300

    Todo listo

--- Webfinal/Miapp/views.py
+++ Webfinal/Miapp/views.py
@@ -1,5 +1,6 @@
 from http.client import HTTPResponse
 from this import d
+from urllib.request import Request
 from django.shortcuts import render, redirect
 from django.template import Context, Template
 from Miapp.forms import Formulario_Insumos, Formulario_celulares, Formulario_hardware, Formulario_software
@@ -9,6 +10,7 @@ from .models import Celulares, Insumos,Hardware,Software
 
 def inicio (request):
     return render(request,"inicio.html")
+
 
 def celulares (request):
     if request.method == "POST":
@@ -89,12 +91,16 @@ def formulario_software (request):
     return render (request, "formulario_software.html", {'mi_formulario': mi_formulario})
 
 def buscar_modelo_cel(request):
-    
-    modelo_buscado=request.GET['modelo_cel']
-    celulares=Celulares.objects.get(modelo_celular=modelo_buscado)
-    return render(request,"busqueda_cel.html",{"celulares":celulares,"modelo":modelo_buscado,})
-    
-    
+        if request.GET['modelo_cel']:
+            modelo_buscado=request.GET['modelo_cel']
+            celulares=Celulares.objects.filter(modelo_celular__icontains=modelo_buscado)
+        
+            return render(request,"busqueda_cel.html",{"celulares":celulares,"modelo":modelo_buscado,})
+        else:
+            respuesta="usted ha ingresado datos en blanco, intente de nuevo"
+        return render(request,'busqueda_cel.html',{"respuesta":respuesta})
+   
+   
 def show_celulares(request):
     lista = Celulares.objects.all()    
     return render(request, "show_cel.html",{"Celulares": lista} )
