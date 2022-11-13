5dc9ffee9c8b50ccfa8bd8aebb7ab2542cba81d0
AdanCP <116222458+AdanCP@users.noreply.github.com>
2022-11-13 12:53:43 -0300

    agregado logout - usuario activo - cierre de sesion en inicio

--- Webfinal/Miapp/urls.py
+++ Webfinal/Miapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from .models import Hardware, Software, Insumos, Celulares
 from .views import agregado, celulares, hardware, inicio, insumos, show_celulares,software,buscar_modelo_cel,show_hard, show_insumos,show_soft,nuevin, registro_usuario,eliminar_celular,show_cel_del,editar_celular,show_cel_edit, login_usuario
 
@@ -18,8 +19,10 @@ urlpatterns = [
     path('nuevin/',nuevin),
     path('formulario_registro_usuario/', registro_usuario, name ="Registro"),
     path ("login_usuario/", login_usuario, name ="Login"),
+    path ("logout_usuario/", LogoutView.as_view (template_name= "logout.html"), name = "Logout"),
     path('show_cel_del',show_cel_del,name="show_cel_del"),#template donde se eliminan los celulares
     path('eliminar_cel/<int:id>',eliminar_celular,name='eliminar_cel'),#eliminacion deregistros de celulares
     path('show_cel_edit',show_cel_edit,name="show_cel_edit"),#template donde se eliminan los celulares
     path('editar_cel/<int:id>',editar_celular,name='editar_cel')
+    
 ]
